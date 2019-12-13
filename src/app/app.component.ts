6ed76e9a9d0cff98b4c0f3df6b2f19d19775e823
Judah Goff <35714383+bgoff1@users.noreply.github.com>
2019-12-13 17:36:40 -0600

    Add receiving cards (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import websocketUrl from '../websocket-config';
+import { Card } from './models/card.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  cards: Card[] = [];
   ngOnInit() {
     const socket = new WebSocket(websocketUrl);
-    socket.onmessage = (message: MessageEvent) => console.log(message);
+    socket.onmessage = (message: MessageEvent) => {
+      this.cards = JSON.parse(message.data);
+      console.log(this.cards);
+    };
     socket.onclose = (event: CloseEvent) => console.log('Websocket Closed!');
   }
 }
